632e9d0fa976bb938b515f32381e6cb70ed67b36
Lakhan Patidar <155709015+lpatida@users.noreply.github.com>
2024-01-11 06:43:37 +0000

    container.config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountEmail = "lakhan@123gmail.com"
+var accountPassword = "789456"
+accountCity = "khargone"
+
+
+// accountId = 8521565 // its not allowed to be chng accountid 
+
+accountEmail = "lakhan@1234.gmail.com"
+accountPassword = "525426"
+accountCity = "mumbai"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+      //    OR
+console.table([accountId, accountEmail, accountPassword, accountCity])
