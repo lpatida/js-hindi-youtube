00d9feaa5e3e42b942313adc3d36f6386396c02c
Lakhan Patidar <155709015+lpatida@users.noreply.github.com>
2024-01-11 07:44:28 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newr version
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+5)
+ //code readability should be high
+console.log("Lakhan")
+
+let name = "lakhan"
+let age = 24
+let ialoggedIn = false
+let state;
+
+
+// DATATYPE
+// number = 2 to power 53
+// bigint 
+// String = ""
+// boolean = true/false
+// null = standalone value 
+// undefined =>
+// symbol => unique
+
+
+
+// Object
+
+
+
+console.log(typeof age);
+console.log(typeof null); // object 
+console.log(typeof undefined);// undefined
+console.log(typeof symbol); // undefined
