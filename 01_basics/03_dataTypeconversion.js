f79311d5d9ac61344aa4a761eae338718d52e4a8
Lakhan Patidar <155709015+lpatida@users.noreply.github.com>
2024-01-22 06:47:26 +0000

    data conversion&Opration

---
+++ 01_basics/03_dataTypeconversion.js
@@ -0,0 +1,34 @@
+let score = "33"
+//"123abc",null, undefined, true,"lakhan" 
+// use only one value at the time 
+// console.log (typeof score);
+// console.log(typeof(score));
+//covert to string  let score ="33"
+
+let valueInNumer = Number(score)
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+//"33"=> 33
+// "123abc" => NuN
+// true => 1 ; false => 0 ;
+
+let isLoggedIn = "Lakhan"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+
+// 1 => true 0 => false
+// "" => false
+// "Lakhan" => true 
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
